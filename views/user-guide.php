54e2cf8fc38e48214616fa2c6e03934a3fdd23f0
John Hughes <john@castlegateit.co.uk>
2017-08-08 12:29:53 +0100

    Add missing user guide

---
+++ views/user-guide.php
@@ -0,0 +1,7 @@
+<p>The search engine optimisation (SEO) fields allow you to customise the text used by search engines to index the site. These are entirely optional. If you do not enter anything in these fields, default values will generated using the page and site title.</p>
+
+<ul>
+    <li><b>Title:</b> the title displayed in the tab or title bar in the browser.</li>
+    <li><b>Heading:</b> a hidden heading made visible to search engines.</li>
+    <li><b>Description:</b> the text displayed in the search engine results listing.</li>
+</ul>
